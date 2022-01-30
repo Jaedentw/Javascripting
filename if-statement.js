7bde43dd6af85a60effabdccf8d0a8be4fd1eb18
Jaeden West <91494277+Bloop180@users.noreply.github.com>
2022-01-30 00:19:57 -0700

    Add files via upload

---
+++ if-statement.js
@@ -0,0 +1,3 @@
+let fruit = "orange"
+if ( fruit => 5 ) {console.log("The fruit name has more than five characters.")} else {console.log("The fruit name has five characters or less.")}
+
---
+++ looping-through-arrays.js
@@ -0,0 +1,3 @@
+const pets =['cat', 'dog', 'rat']
+for (let i =0; i <= (pets.length-1); i++) { pets[i] = pets[i] + 's'}
+console.log(pets)
